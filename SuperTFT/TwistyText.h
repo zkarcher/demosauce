13ff72864001fc61618a3fdd83dcb12dba6e43f0
Zach Archer <zkarcher@gmail.com>
2015-11-23 15:53:03 -0800

    TwistyText thickness

--- SuperTFT/TwistyText.h
+++ SuperTFT/TwistyText.h
@@ -29,6 +29,7 @@ const uint_fast8_t TEXT_TABLE_LENGTH = 17;
 const uint_fast8_t TEXT_PIXEL_WIDTH = 10;
 const float TEXT_PIXEL_HEIGHT = 10.0f;
 
+const float TEXT_SPIN_SPEED = 0.007f;
 const float TWIST_AMOUNT = 0.1f;
 
 // This function (which actually gets 'inlined' anywhere it's called)
@@ -53,19 +54,20 @@ void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 
 	uint_fast16_t h_2 = (h>>1);
 
-	tt._phase += frameParams.timeMult * 0.007;
+	tt._phase += frameParams.timeMult * TEXT_SPIN_SPEED;
 	if( tt._phase > 1.0f ) tt._phase -= 1.0f;
 
 	for( uint_fast8_t c=0; c<TEXT_TABLE_LENGTH; c++ ) {
 		uint_fast16_t left = c * TEXT_PIXEL_WIDTH;
 
 		// Erase the background
-		tft.fillRect( left, h_2 - 4*TEXT_PIXEL_HEIGHT, TEXT_PIXEL_WIDTH, 8*TEXT_PIXEL_HEIGHT, 0x0 );
+		tft.fillRect( left, h_2 - 5*TEXT_PIXEL_HEIGHT, TEXT_PIXEL_WIDTH, 10*TEXT_PIXEL_HEIGHT, 0x0 );
 
 		float angle = (tt._phase * M_PI) + c*TWIST_AMOUNT;
 		if( angle > M_PI ) angle -= M_PI;
 
 		float cosAngle = cos( angle );
+		float cosAngleAbs = abs( cosAngle );
 		float cosAngleNorm = (cosAngle+1.0)*0.5f;
 		float sinAngle = sin( angle );
 
@@ -74,6 +76,10 @@ void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 			lerp8( 0x44, 0xff, cosAngleNorm ),
 			lerp8( 0x44, 0xbb, cosAngleNorm )
 		);
+
+		uint_fast16_t sideColor = tft.color565( 0xff * cosAngleAbs, 0x88 * cosAngleAbs, 0 );
+
+		uint_fast16_t sideHeight = abs( cosAngle ) * TEXT_PIXEL_HEIGHT;
 
 		uint_fast8_t colByte = pgm_read_byte(&TEXT_TABLE[c]);
 
@@ -91,11 +97,20 @@ void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 			} else if( inRect && !isSolid ) {
 				inRect = false;
 
-				uint_fast16_t top = h_2 + ((int_fast8_t)(topBit-4) * TEXT_PIXEL_HEIGHT) * sinAngle;
+				uint_fast16_t top = h_2 + ((int_fast8_t)(topBit-4) * TEXT_PIXEL_HEIGHT)*sinAngle - (TEXT_PIXEL_HEIGHT*cosAngle);
 				uint_fast16_t height = ((bit-topBit) * TEXT_PIXEL_HEIGHT) * sinAngle;
 
-				// Draw the pixels
+				// Draw the pixel column
 				tft.fillRect( left, top, TEXT_PIXEL_WIDTH, height, color );
+
+				// Draw the "side" hanging below, or above, depending on the twist angle
+				if( cosAngle > 0.0f ) {
+					// Below: (text is facing up)
+					tft.fillRect( left, top+height, TEXT_PIXEL_WIDTH, sideHeight, sideColor );
+				} else {
+					// Above: (text is facing down)
+					tft.fillRect( left, top-sideHeight, TEXT_PIXEL_WIDTH, sideHeight, sideColor );
+				}
 			}
 		}
 	}
