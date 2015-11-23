ee7b3102e3905f50b2f0478afbc61e321da8c2a2
Zach Archer <zkarcher@gmail.com>
2015-11-23 15:41:37 -0800

    TwistyText progress

--- SuperTFT/TwistyText.h
+++ SuperTFT/TwistyText.h
@@ -27,7 +27,9 @@ const uint_fast8_t PROGMEM TEXT_TABLE[]  = {
 const uint_fast8_t TEXT_TABLE_LENGTH = 17;
 
 const uint_fast8_t TEXT_PIXEL_WIDTH = 10;
-const uint_fast8_t TEXT_PIXEL_HEIGHT = 10;
+const float TEXT_PIXEL_HEIGHT = 10.0f;
+
+const float TWIST_AMOUNT = 0.1f;
 
 // This function (which actually gets 'inlined' anywhere it's called)
 // exists so that gammaTable can reside out of the way down here in the
@@ -46,20 +48,32 @@ void twistyText_setup( ILI9341_t3 tft ) {
 }
 
 void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
-  uint_fast16_t w = (uint_fast16_t)tft.width();
+  //uint_fast16_t w = (uint_fast16_t)tft.width();
   uint_fast16_t h = (uint_fast16_t)tft.height();
 
-	uint_fast8_t h_2 = (h>>1);
+	uint_fast16_t h_2 = (h>>1);
 
-	tt._phase += frameParams.timeMult * 0.02;
+	tt._phase += frameParams.timeMult * 0.007;
+	if( tt._phase > 1.0f ) tt._phase -= 1.0f;
 
 	for( uint_fast8_t c=0; c<TEXT_TABLE_LENGTH; c++ ) {
-		float angle = tt._phase + c*0.02f;
+		uint_fast16_t left = c * TEXT_PIXEL_WIDTH;
+
+		// Erase the background
+		tft.fillRect( left, h_2 - 4*TEXT_PIXEL_HEIGHT, TEXT_PIXEL_WIDTH, 8*TEXT_PIXEL_HEIGHT, 0x0 );
+
+		float angle = (tt._phase * M_PI) + c*TWIST_AMOUNT;
+		if( angle > M_PI ) angle -= M_PI;
+
 		float cosAngle = cos( angle );
+		float cosAngleNorm = (cosAngle+1.0)*0.5f;
 		float sinAngle = sin( angle );
 
-		uint_fast8_t bright = (uint_fast8_t)( cosAngle * 0xff );
-		uint_fast16_t color = tft.color565( (bright>>2), bright, (bright*bright)>>8 );
+		uint_fast16_t color = tft.color565(
+			lerp8( 0x33, 0x88, cosAngleNorm ),
+			lerp8( 0x44, 0xff, cosAngleNorm ),
+			lerp8( 0x44, 0xbb, cosAngleNorm )
+		);
 
 		uint_fast8_t colByte = pgm_read_byte(&TEXT_TABLE[c]);
 
@@ -67,7 +81,9 @@ void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 		boolean inRect = false;
 		uint_fast8_t topBit;
 		for( uint_fast8_t bit=0; bit<=8; bit++ ) {
+
 			boolean isSolid = (boolean)( colByte & (0x1 << bit) );
+
 			if( !inRect && isSolid ) {
 				inRect = true;
 				topBit = bit;
@@ -75,12 +91,8 @@ void twistyText_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 			} else if( inRect && !isSolid ) {
 				inRect = false;
 
-				uint_fast16_t left = c * TEXT_PIXEL_WIDTH;
-				uint_fast16_t top = h_2 - ((topBit-4) * TEXT_PIXEL_HEIGHT) * sinAngle;
-				uint_fast16_t height = h_2 - ((bit-topBit) * TEXT_PIXEL_HEIGHT) * sinAngle;
-
-				// Erase the background
-				tft.fillRect( left, h_2 - 4*TEXT_PIXEL_HEIGHT, TEXT_PIXEL_WIDTH, 8*TEXT_PIXEL_HEIGHT, 0x0 );
+				uint_fast16_t top = h_2 + ((int_fast8_t)(topBit-4) * TEXT_PIXEL_HEIGHT) * sinAngle;
+				uint_fast16_t height = ((bit-topBit) * TEXT_PIXEL_HEIGHT) * sinAngle;
 
 				// Draw the pixels
 				tft.fillRect( left, top, TEXT_PIXEL_WIDTH, height, color );
