762a449c70b8b9363a1da34928c58ca895fe4715
Zach Archer <zkarcher@gmail.com>
2015-11-23 19:42:47 -0800

    Pretty good yellow plasma

--- SuperTFT/Plasma.h
+++ SuperTFT/Plasma.h
@@ -5,7 +5,7 @@
 #include "ILI9341_t3.h"
 #include "MathUtil.h"
 
-const float PLASMA_SPEED = 0.018;
+const float PLASMA_SPEED = 0.02;
 const uint_fast8_t PLASMA_DITHER = 8;
 const uint_fast16_t PLASMA_MARGIN = 25;
 
@@ -40,23 +40,25 @@ void plasma_perFrame( ILI9341_t3 tft, FrameParams frameParams ) {
 		(int_fast16_t)(w/2 + (sin(plasma._phase*0.57f)*(w/2-PLASMA_MARGIN) )),
 		(int_fast16_t)(h/2 + (sin(plasma._phase*0.23f)*(h/2-PLASMA_MARGIN) ))
 	};
-	//Point16 p1 = (Point16){ w * (sin(plasma._phase*0.78f)+1.0f)*0.5f, h * (sin(plasma._phase*0.42f)+1.0f)*0.5f };
+	Point16 p1 = (Point16){
+		(int_fast16_t)(w/2 + (cos(plasma._phase*0.78f)*(w/2-PLASMA_MARGIN) )),
+		(int_fast16_t)(h/2 + (cos(plasma._phase*0.42f)*(h/2-PLASMA_MARGIN) ))
+	};
 
-	float audioPower = (1.0f - frameParams.audioMean*0.999f);
+	float audioPower = frameParams.audioMean;
 
 	for( int_fast16_t x=0; x<w; x+=PLASMA_DITHER ) {
 		for( int_fast16_t y=plasma._ditherY; y<h; y+=PLASMA_DITHER ) {
 			Point16 d0 = (Point16){ p0.x - x, p0.y - y };
-			//Point16 d1 = (Point16){ p1.x - x, p1.y - y };
+			Point16 d1 = (Point16){ p1.x - x, p1.y - y };
 
 			//uint_fast16_t distance = sqrt( d0.x*d0.x + d0.y*d0.y );// * sqrt( d1.x*d1.x + d1.y*d1.y );	// SLOW
-			uint_fast16_t distance = abs(d0.x*d0.y);	// pretty good
+			//uint_fast16_t distance = abs( d0.x*d0.y );	// pretty good
+			uint_fast16_t distance = abs( d0.x*d0.y + d1.x*d1.y );
 
-			uint_fast8_t green = (uint_fast16_t)((distance >> 3) * audioPower) & 0xff;
-			uint_fast8_t red = green;
-//			uint_fast8_t red = (distance >> 2) & 0xff;
+			uint_fast8_t bright = lerp8( (uint_fast16_t)(distance >> 4) & 0xff, 0xff, audioPower );
 
-			uint_fast16_t color = tft.color565( red, green, 0 );
+			uint_fast16_t color = tft.color565( bright, bright, 0 );
 
 			tft.drawFastHLine( x, y, PLASMA_DITHER, color );
 		}
